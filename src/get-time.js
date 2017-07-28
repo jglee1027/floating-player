060c6e851b13fc5aab8c9340adcba1e0a46c8ef1
Gabriel Santos <gabrielbarros@users.noreply.github.com>
2017-07-28 07:13:59 -0300

    Remove content.js and add get-time.js

---
+++ src/get-time.js
@@ -0,0 +1,17 @@
+var $video = document.getElementsByTagName('video')[0];
+var time;
+
+if ($video) {
+    try {
+        // Pause video
+        $video.pause();
+
+        // Get current video time
+        time = Math.floor($video.currentTime);
+    }
+    catch(e) {
+        console.log(e);
+    }
+}
+
+time;
