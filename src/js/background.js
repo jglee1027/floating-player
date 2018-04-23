98b89e6c7639d423e33b684987402608ff45b840
Gabriel Santos <gabrielbarros@users.noreply.github.com>
2018-04-23 05:09:42 -0300

    Remove import old settings

--- src/js/background.js
+++ src/js/background.js
@@ -2,79 +2,8 @@ if (options.context) {
     addContextMenu();
 }
 
-// Import old settings (this will be removed soon)
-if (localStorage.getItem('ok')) {
-    var key;
-    var value;
-    var isOption;
-
-    for (var i in localStorage) {
-        if (localStorage.hasOwnProperty(i)) {
-            isOption = i.substr(0, 1) === '_';
-
-            if (isOption) {
-                key = i.substr(1);
-                value = localStorage.getItem(i);
-
-                // Update old keys/values
-                switch (key) {
-                    case 'api':
-                        key = 'youtubeApi';
-                        break;
-
-                    case 'fix':
-                        key = 'fixPopup';
-                        break;
-
-                    case 'hmargin':
-                        key = 'horizontalMargin';
-                        break;
-
-                    case 'vmargin':
-                        key = 'verticalMargin';
-                        break;
-
-                    case 'color':
-                        if (value === '1') {
-                            value = 'red';
-                        }
-                        else {
-                            value = 'white';
-                        }
-                        break;
-                }
-
-                // Boolean true
-                if (value === 'true') {
-                    setOption(key, true);
-                }
-                // Boolean false
-                else if (value === 'false') {
-                    setOption(key, false);
-                }
-                // Number
-                else if (key !== 'title' && (value - 0 == value &&
-                    ('' + value).trim().length > 0)) {
-
-                    setOption(key, +value);
-                }
-                // String
-                else {
-                    setOption(key, value);
-                }
-
-                localStorage.removeItem(i);
-            }
-        }
-    }
-
-    localStorage.removeItem('ok');
-    localStorage.setItem('installed', Date.now());
-    getAllOptions();
-}
-
 // 1st time
-else if (!localStorage.getItem('installed')) {
+if (!localStorage.getItem('installed')) {
     localStorage.setItem('installed', Date.now());
     showInstructions();
 }
